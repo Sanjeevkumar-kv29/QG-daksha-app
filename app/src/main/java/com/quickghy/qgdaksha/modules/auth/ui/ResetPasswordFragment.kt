43176a81f73e96b0730e71d77571a8d3e9bf3766
Shubham Rimjha <shubhamrim@gmail.com>
2021-06-18 16:39:38 +0530

    Structure/skeleton of the auth module.(nav_auth made)

---
+++ app/src/main/java/com/quickghy/qgdaksha/modules/auth/ui/ResetPasswordFragment.kt
@@ -0,0 +1,19 @@
+package com.quickghy.qgdaksha.modules.auth.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.quickghy.qgdaksha.R
+
+class ResetPasswordFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+    }
+
+}
