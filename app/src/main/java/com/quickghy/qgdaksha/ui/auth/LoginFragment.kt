5b590e68757a177bb9a63ee6c493cafbce2da66a
Shubham Rimjha <shubhamrim@gmail.com>
2021-06-22 23:04:07 +0530

    commit thode change login k code me

--- app/src/main/java/com/quickghy/qgdaksha/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/quickghy/qgdaksha/ui/auth/LoginFragment.kt
@@ -7,22 +7,28 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.quickghy.qgdaksha.R
 import com.quickghy.qgdaksha.databinding.FragmentLoginBinding
-import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment(), AuthStateListener.LoginStateListener {
+
+    lateinit var viewModel: AuthViewModel
+    lateinit var binding: FragmentLoginBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding: FragmentLoginBinding = DataBindingUtil.setContentView(requireActivity(),R.layout.fragment_login)
-        val viewModel = ViewModelProviders.of(this@LoginFragment).get(AuthViewModel::class.java)
+        binding = DataBindingUtil.setContentView(requireActivity(), R.layout.fragment_login)
+//        because the viewmodel was first created in the main activity.
+        activity.let {
+            viewModel = ViewModelProvider(it!!).get(AuthViewModel::class.java)
+        }
+//        ViewModelProviders.of(this@LoginFragment).get(AuthViewModel::class.java)
 
         binding.viewmodel = viewModel
 
@@ -50,7 +56,7 @@ class LoginFragment : Fragment(), AuthStateListener.LoginStateListener {
 
     override fun onLoginSuccess(loginResponse: String) {
 
-            Toast.makeText(context, loginResponse, Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, loginResponse, Toast.LENGTH_SHORT).show()
 
 
     }
