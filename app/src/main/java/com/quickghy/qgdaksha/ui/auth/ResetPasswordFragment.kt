b32dcab9b11bb442e022eec02ea1874971a0ea0c
Shubham Rimjha <shubhamrim@gmail.com>
2021-06-23 12:25:04 +0530

    thoda layout ka kaam kia h

--- app/src/main/java/com/quickghy/qgdaksha/ui/auth/ResetPasswordFragment.kt
+++ app/src/main/java/com/quickghy/qgdaksha/ui/auth/ResetPasswordFragment.kt
@@ -4,16 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.quickghy.qgdaksha.R
+import com.quickghy.qgdaksha.databinding.FragmentForgotPasswordBinding
 
 class ResetPasswordFragment : Fragment() {
+
+    lateinit var viewModel: AuthViewModel
+    lateinit var binding: FragmentForgotPasswordBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+        // Inflate the layout for this fragment
+        binding =
+            DataBindingUtil.setContentView(requireActivity(), R.layout.fragment_reset_password)
+
+        activity.let {
+            viewModel = ViewModelProvider(it!!).get(AuthViewModel::class.java)
+        }
+        binding.forgotviewmodel = viewModel
+
+        viewModel.forgotPasswordStateListner = this
+
+
+
+        return binding.root
     }
 
 }
