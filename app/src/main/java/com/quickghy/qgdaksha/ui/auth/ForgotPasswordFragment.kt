4993b7a15d130cc9aa505c364a8bbfc95c89ab14
Shubham Rimjha <shubhamrim@gmail.com>
2021-07-02 13:56:39 +0530

    correction.

--- app/src/main/java/com/quickghy/qgdaksha/ui/auth/ForgotPasswordFragment.kt
+++ app/src/main/java/com/quickghy/qgdaksha/ui/auth/ForgotPasswordFragment.kt
@@ -19,7 +19,7 @@ class ForgotPasswordFragment : Fragment(), AuthStateListener.ForgotPasswordState
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val view = inflater.inflate(R.layout.fragment_forgot_password, container, false)
 
         activity.let {
@@ -33,7 +33,7 @@ class ForgotPasswordFragment : Fragment(), AuthStateListener.ForgotPasswordState
             false
         )
 
-        binding.forgotviewmodel = viewModel
+        binding.viewmodel = viewModel
         viewModel.forgotPasswordStateListner = this
         return binding.root
     }
